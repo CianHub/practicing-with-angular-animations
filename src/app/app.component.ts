2724ef98403e9d890116099ddf78a1ce837a2265
Cian O'Gralaigh <Cian.OGralaigh@version1.com>
2019-08-20 11:50:47 +0100

    add states to animation, set default state, set animation value to be state property

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from "@angular/core";
-import { trigger } from "@angular/animations";
+import { trigger, state, style } from "@angular/animations";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
-  animations: [trigger("divState", [])]
+  animations: [
+    trigger("divState", [
+      state(
+        "normal",
+        style({ backgroundColor: "red", transform: "translateX(0)" })
+      ),
+      state(
+        "highlighted",
+        style({ backgroundColor: "yellow", transform: "translateX(100px)" })
+      )
+    ])
+  ]
 })
 export class AppComponent {
+  state = "normal";
   list = ["Milk", "Sugar", "Bread"];
 
   onAdd(item) {
