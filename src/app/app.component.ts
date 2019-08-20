d67741df6dccc6a44bd182dd8395307048af06f5
Cian O'Gralaigh <Cian.OGralaigh@version1.com>
2019-08-20 11:44:45 +0100

    Add animation array and trigger function, add animation to div

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from "@angular/core";
+import { trigger } from "@angular/animations";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  styleUrls: ["./app.component.css"],
+  animations: [trigger("divState", [])]
 })
 export class AppComponent {
   list = ["Milk", "Sugar", "Bread"];
