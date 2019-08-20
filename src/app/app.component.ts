0f5230fc5fcef6b27434e156ec0af5592157f61c
Cian O'Gralaigh <Cian.OGralaigh@version1.com>
2019-08-20 11:39:27 +0100

    project setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'angular-animations';
+  list = ["Milk", "Sugar", "Bread"];
+
+  onAdd(item) {
+    this.list.push(item);
+  }
 }
