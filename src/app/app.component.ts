1a73639bfc7ea41243f96ebe542390fcbdd737e1
Cian O'Gralaigh <Cian.OGralaigh@version1.com>
2019-08-20 13:23:16 +0100

    install bootstrap, add more animations, add void animation, add detail to transitions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from "@angular/core";
-import { trigger, state, style } from "@angular/animations";
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate
+} from "@angular/animations";
 
 @Component({
   selector: "app-root",
@@ -14,13 +20,62 @@ import { trigger, state, style } from "@angular/animations";
       state(
         "highlighted",
         style({ backgroundColor: "yellow", transform: "translateX(100px)" })
-      )
+      ),
+      transition("normal => highlighted", animate(300)),
+      transition("highlighted => normal", animate(500))
+    ]),
+    trigger("newDivState", [
+      state(
+        "normal",
+        style({ backgroundColor: "red", transform: "translateX(0)" })
+      ),
+      state(
+        "highlighted",
+        style({ backgroundColor: "yellow", transform: "translateX(100px)" })
+      ),
+      state(
+        "shrunken",
+        style({
+          backgroundColor: "green",
+          transform: "translateX(0px) scale(0.5)"
+        })
+      ),
+      transition("normal => highlighted", animate(200)),
+      transition("highlighted => normal", animate(800)),
+      transition("shrunken <=> *", [
+        style({ backgroundColor: "orange" }),
+        animate(1000, style({ borderRadius: "50px" })),
+        animate(500)
+      ])
+    ]),
+    trigger("list1", [
+      state("in", style({ opacity: "1", transform: "translateX(0)" })),
+      transition("void => *", [
+        style({ opacity: "0", transform: "translateX(-100px)" }),
+        animate(300)
+      ])
     ])
   ]
 })
 export class AppComponent {
   state = "normal";
+  newState = "normal";
+
   list = ["Milk", "Sugar", "Bread"];
+
+  onAnimate() {
+    this.state == "normal"
+      ? (this.state = "highlighted")
+      : (this.state = "normal");
+
+    this.newState == "normal"
+      ? (this.newState = "highlighted")
+      : (this.newState = "normal");
+  }
+
+  onShrink() {
+    this.newState = "shrunken";
+  }
 
   onAdd(item) {
     this.list.push(item);
